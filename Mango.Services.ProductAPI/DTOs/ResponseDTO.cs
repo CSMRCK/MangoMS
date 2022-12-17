66bfb2549ca14f11ee733cccde9e6c6d9d4a7e2c
CSMRCK <42943919+CSMRCK@users.noreply.github.com>
2022-12-17 22:57:28 +0200

    Added controller for ProductAPI

--- Mango.Services.ProductAPI/DTOs/ResponseDTO.cs
+++ Mango.Services.ProductAPI/DTOs/ResponseDTO.cs
@@ -2,7 +2,7 @@
 {
     public class ResponseDTO
     {
-        public bool isSuccess { get; set; } = true;
+        public bool IsSuccess { get; set; } = true;
         public object Result { get; set; }
         public string DisplayMessage { get; set; } = "";
         public List<string> ErrorMessages { get; set; }
