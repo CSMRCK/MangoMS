87a45df281177ee7811670c42412cbc6a4af5815
CSMRCK <42943919+CSMRCK@users.noreply.github.com>
2022-12-17 19:25:11 +0200

    Added Product repository interface

--- Mango.Services.ProductAPI/Repository/IProductRepository.cs
+++ Mango.Services.ProductAPI/Repository/IProductRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace Mango.Services.ProductAPI.Repository
+﻿using Mango.Services.ProductAPI.DTOs;
+using System.Collections;
+
+namespace Mango.Services.ProductAPI.Repository
 {
     public interface IProductRepository
     {
+        Task<IEnumerable<ProductDTO>> GetProducts();
+        Task<ProductDTO> GetProductById(int productId);
+        Task<ProductDTO> CreateUpdateProduct(ProductDTO productDTO);
+        Task<bool> DeleteProduct(int productId);
     }
 }
