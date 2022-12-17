a7528bad591cfaf852917c31999a1503c5d86cd5
CSMRCK <42943919+CSMRCK@users.noreply.github.com>
2022-12-17 19:20:26 +0200

    Added Product and Response DTOs

---
+++ Mango.Services.ProductAPI/DTOs/ResponseDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Mango.Services.ProductAPI.DTOs
+{
+    public class ResponseDTO
+    {
+        public bool isSuccess { get; set; } = true;
+        public object Result { get; set; }
+        public string DisplayMessage { get; set; } = "";
+        public List<string> ErrorMessages { get; set; }
+    }
+}
