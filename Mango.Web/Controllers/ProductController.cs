6fa7b29467be3a2ae2540e1d3cfe1b5cc72994d4
CSMRCK <42943919+CSMRCK@users.noreply.github.com>
2022-12-18 17:13:40 +0200

    Added rest crud controllers and views

--- Mango.Web/Controllers/ProductController.cs
+++ Mango.Web/Controllers/ProductController.cs
@@ -2,6 +2,8 @@
 using Mango.Web.Services.IServices;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace Mango.Web.Controllers
 {
@@ -17,11 +19,86 @@ namespace Mango.Web.Controllers
         {
             List<ProductDTO> list = new();
             var resonse = await _productService.GetAllProductsAsync<ResponseDTO>();
-            if(resonse!=null && resonse.IsSuccess) 
+            if (resonse != null && resonse.IsSuccess)
             {
                 list = JsonConvert.DeserializeObject<List<ProductDTO>>(Convert.ToString(resonse.Result));
             }
             return View(list);
+        }
+
+        public async Task<IActionResult> ProductEdit()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ProductIndex(ProductDTO model)
+        {
+            if (ModelState.IsValid)
+            {
+                var resonse = await _productService.CreateProductAsync<ResponseDTO>(model);
+                if (resonse != null && resonse.IsSuccess)
+                {
+                    return RedirectToAction(nameof(ProductIndex));
+                }
+            }
+            return View(model);
+
+        }
+
+        public async Task<IActionResult> ProductEdit(int productId)
+        {
+            var resonse = await _productService.GetProductByIdAsync<ResponseDTO>(productId);
+            if (resonse != null && resonse.IsSuccess)
+            {
+                ProductDTO model = JsonConvert.DeserializeObject<ProductDTO>(Convert.ToString(resonse.Result));
+                return View(model);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ProductEdit(ProductDTO model)
+        {
+            if (ModelState.IsValid)
+            {
+                var resonse = await _productService.UpdateProductAsync<ResponseDTO>(model);
+                if (resonse != null && resonse.IsSuccess)
+                {
+                    return RedirectToAction(nameof(ProductIndex));
+                }
+            }
+            return View(model);
+
+        }
+        public async Task<IActionResult> ProductDelete(int productId)
+        {
+            var resonse = await _productService.GetProductByIdAsync<ResponseDTO>(productId);
+            if (resonse != null && resonse.IsSuccess)
+            {
+                ProductDTO model = JsonConvert.DeserializeObject<ProductDTO>(Convert.ToString(resonse.Result));
+                return View(model);
+            }
+
+            return NotFound();
+        }
+
+        [HttpDelete]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ProductDelete(ProductDTO model)
+        {
+            if (ModelState.IsValid)
+            {
+                var resonse = await _productService.DeleteProductAsync<ResponseDTO>(model.ProductId);
+                if (resonse != null && resonse.IsSuccess)
+                {
+                    return RedirectToAction(nameof(ProductIndex));
+                }
+            }
+            return View(model);
+
         }
     }
 }
